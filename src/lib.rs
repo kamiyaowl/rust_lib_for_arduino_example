b16ef308d30b9c74501807d9311fe03801a1e237
K <kamiyaowl@gmail.com>
2020-05-13 00:41:40 +0900

    create sample library

---
+++ src/lib.rs
@@ -0,0 +1,20 @@
+#![no_std]
+#![feature(lang_items, core_intrinsics)]
+
+use core::intrinsics;
+use core::panic::PanicInfo;
+
+#[panic_handler]
+#[no_mangle]
+pub fn panic(_info: &PanicInfo) -> ! {
+    unsafe { intrinsics::abort() }
+}
+
+#[lang = "eh_personality"]
+#[no_mangle]
+pub extern "C" fn rust_eh_personality() {}
+
+#[no_mangle]
+pub unsafe extern "C" fn example_add(a: u32, b: u32) -> u32 {
+    a + b
+}
