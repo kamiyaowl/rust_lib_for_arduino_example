0dbe8f931aecd3281c32fdab2920164d8b539aee
K <kamiyaowl@gmail.com>
2020-05-13 03:13:06 +0900

    add screenshot

--- build.sh
+++ build.sh
@@ -26,9 +26,10 @@ echo "copy target/<target arch>/release/lib$LIB_NAME.a"
 cp "./target/thumbv6m-none-eabi/release/lib$LIB_NAME.a"    "$DST_DIR/src/cortex-m0/"
 cp "./target/thumbv6m-none-eabi/release/lib$LIB_NAME.a"    "$DST_DIR/src/cortex-m0plus/"
 cp "./target/thumbv7m-none-eabi/release/lib$LIB_NAME.a"    "$DST_DIR/src/cortex-m3/"
-cp "./target/thumbv7em-none-eabihf/release/lib$LIB_NAME.a" "$DST_DIR/src/cortex-m4/" # for Cortex-M4F/M7F
-cp "./target/thumbv7em-none-eabihf/release/lib$LIB_NAME.a" "$DST_DIR/src/cortex-m7/" # for Cortex-M4F/M7F
-# cp "./target/thumbv7em-none-eabi/release/lib$LIB_NAME.a" "$DST_DIR/src/cortex-m4/" # for Cortex-M4/M7
+cp "./target/thumbv7em-none-eabihf/release/lib$LIB_NAME.a" "$DST_DIR/src/cortex-m4/" # for Cortex-M4F
+cp "./target/thumbv7em-none-eabihf/release/lib$LIB_NAME.a" "$DST_DIR/src/cortex-m7/" # for Cortex-M7F
+# cp "./target/thumbv7em-none-eabi/release/lib$LIB_NAME.a" "$DST_DIR/src/cortex-m4/" # for Cortex-M4
+# cp "./target/thumbv7em-none-eabi/release/lib$LIB_NAME.a" "$DST_DIR/src/cortex-m7/" # for Cortex-M7
 
 echo "copy $LIB_NAME.h"
 cp "./$LIB_NAME.h" "$DST_DIR/src/"
